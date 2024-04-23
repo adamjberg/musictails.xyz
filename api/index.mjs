7699397bc6b9b57bb745e26b99f68bf12ad75075
Adam <adam@xyzdigital.com>
2024-04-22 21:34:17 -0700

    Add top level remix build and serve via api

--- api/index.mjs
+++ api/index.mjs
@@ -1,7 +1,15 @@
+import { createRequestHandler } from "@remix-run/express";
 import express from "express";
 
+import * as build from "../build/server/index.js";
+
 const app = express();
+app.use(express.static("../build/client"));
 
-app.use(express.static("../fe/public"))
+// app.use(express.static("../fe/public"));
 
-app.listen(8000);
+app.all("*", createRequestHandler({ build }));
+
+app.listen(3000, () => {
+  console.log("App listening on http://localhost:3000");
+});
