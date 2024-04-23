17f533bde9d0e1fbdb082678d4be65026c9110ee
Adam <adam@xyzdigital.com>
2024-04-23 08:07:47 -0400

    Add basic amplitude animation

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,7 @@ export default function App() {
     <html>
       <head>
         <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <Meta />
         <Links />
       </head>
--- app/routes/2024-04-22._index.tsx
+++ app/routes/2024-04-22._index.tsx
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 export default function Index() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (!ctx) {
+        return;
+      }
+
+      const resizeHandler = () => {
+        const parentWidth = canvas.parentElement?.clientWidth || 0;
+        const width = Math.min(parentWidth, 600);
+        canvas.width = width;
+        canvas.height = width;
+      };
+
+      let animationFrameId = 0;
+
+      const drawCircle = () => {
+        const currentTime = new Date().getTime();
+        const animationDurationMs = 5000;
+        const elapsedTime = currentTime % animationDurationMs;
+        const percent = elapsedTime / animationDurationMs;
+
+        const width = canvas.width;
+        const height = canvas.height;
+        const radius = 6;
+
+        const centerX = width / 2;
+        const centerY = height / 2;
+
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        ctx.beginPath();
+
+        let maxAnimationHeight = canvas.height / 2;
+        const amplitude = Math.cos(2 * Math.PI * percent);
+        let y = centerY + amplitude * maxAnimationHeight;
+
+        ctx.arc(centerX, y, radius, 0, 2 * Math.PI);
+        ctx.fillStyle = "#FFFFFF";
+        ctx.fill();
+
+        animationFrameId = requestAnimationFrame(drawCircle);
+      };
+
+      drawCircle();
+
+      resizeHandler();
+      window.addEventListener("resize", resizeHandler);
+
+      return () => {
+        cancelAnimationFrame(animationFrameId);
+        window.removeEventListener("resize", resizeHandler);
+      };
+    }
+  }, []);
   return (
     <div>
-      <h1>2024-04-22</h1>
-      <p>Start the post</p>
-      <p>How good is auto...now?</p>
+      <h1>Animating Sound: Amplitude</h1>
+      <canvas ref={canvasRef}></canvas>
     </div>
   );
 }
